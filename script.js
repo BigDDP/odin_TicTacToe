b04453d0164c69a7b33b63a0dff55e7533fd754c
Ben Freijah <ben@launchkeylab.com>
2026-02-06 09:21:56 +1100

    feat: Refactored code - Player & Game Prop object factories - better defined game factory functions

--- script.js
+++ script.js
@@ -5,115 +5,129 @@ const init = (() => {
     const restartBtn = document.getElementById("restartBtn");
     const board = document.querySelector(".board");
 
-    const winCondition = [
-        [1,2,3], [4,5,6], [7,8,9], [1,4,7],
-        [2,5,8], [3,6,9], [1,5,9], [3,5,7]
-    ];
+    allCells.forEach((cell) => {
+        cell.addEventListener("click", () => {
+            gameController(cell);
+        });
+    });
 
-    const state = {
-        player1: undefined,
-        player2: undefined,
-        currentPlayer: "O",
-        gameOver: false,
-        gameBoard: [
-            {id: 1, value: ""},{id: 2, value: ""},{id: 3, value: ""},
-            {id: 4, value: ""},{id: 5, value: ""},{id: 6, value: ""},
-            {id: 7, value: ""},{id: 8, value: ""},{id: 9, value: ""}
-        ]
-    };
-
-    const reset = () => {
-        state.gameOver = false;
-        state.currentPlayer = "O";
-        state.gameBoard = state.gameBoard.map(v => ({ ...v, value: ""}));
-        allCells.forEach(cell => (cell.textContent= ""));
-        playerElement.textContent = state.player1 + ": O";
-    };
+    restartBtn.addEventListener("click", () => {
+        gameBoard.reset();
+    });
 
     return {
         playerElement, 
         allCells, 
-        winCondition, 
-        board,
-        restartBtn,
         form,
-        state,
-        reset
+        restartBtn,
+        board
     };
 })();
 
+const gameBoard = (() => {
+    const state = {
+        board: [
+            {id: 1, value: ""},{id: 2, value: ""},{id: 3, value: ""},
+            {id: 4, value: ""},{id: 5, value: ""},{id: 6, value: ""},
+            {id: 7, value: ""},{id: 8, value: ""},{id: 9, value: ""}
+        ],
+        properties: null
+    };
 
-init.allCells.forEach((cell) => {
-    cell.addEventListener("click", () => {
-        gameObj(cell);
-    });
-});
-
-init.restartBtn.addEventListener("click", () => {
-    init.reset();
-})
-
-const formCheck = (() => {
     init.form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        const formData = new FormData(e.target);
+        const fd = new FormData(e.target);
 
-        init.state.player1 = formData.get("player1");
-        init.state.player2 = formData.get("player2");
+        let player1 = playerObject("Player 1", "O", fd.get("player1"));
+        let player2 = playerObject("Player 2", "X", fd.get("player2"));
+
+        state.properties = controller(player1, player2);
 
         init.board.style.display = "grid";
         init.restartBtn.style.display = "block";
         init.form.style.display = "none";
 
-        init.playerElement.textContent = `${init.state.player1}: ${init.state.currentPlayer}`;
+        init.playerElement.textContent = `${player1.name}: ${player1.marker}`;
         init.playerElement.style.display = "block";
     });
+
+    const setMark = (player) => {
+        let nextPlayer = player.marker === "O" ? state.properties.p2 : state.properties.p1;
+
+        state.properties.currentPlayer = nextPlayer
+        init.playerElement.textContent = `${nextPlayer.name}: ${nextPlayer.marker}`  
+    };
+
+    const reset = () => {
+        state.board = state.board.map(v => ({ ...v, value: ""}));
+        state.properties.gameOver = false;
+        state.properties.currentPlayer = state.properties.p1;
+        init.allCells.forEach(cell => (cell.textContent= ""));
+        init.playerElement.textContent = 
+            `${state.properties.p1.name}: ${state.properties.p1.marker}`;
+    };
+
+    return { state, setMark, reset };
 })();
 
-const gameObj = function(cell) {
-            
-    if (init.state.gameOver === true) return;
-    if (init.state.gameBoard[cell.id-1].value !== "") return;
+function playerObject(value, marker, name) {
+    return {
+        value: value,
+        marker: marker,
+        name: name
+    }
+}
 
-    init.state.gameBoard[cell.id-1].value = init.state.currentPlayer;
-
-    cell.textContent = init.state.currentPlayer;
-    
-    if (winCheck.winner(init.state.currentPlayer)) return;
-
-    playerObj(init.state.currentPlayer);
-
-    return ;
+function controller(player1, player2) {
+    return {
+        currentPlayer: player1,
+        gameOver: false,
+        p1: player1,
+        p2: player2
+    };
 };
 
-function playerObj(player) {
+const gameController = (cell) => {
+    let board = gameBoard.state.board;
+    let props = gameBoard.state.properties;
+
+    if (props.gameOver === true) return;
+    if (board[cell.id-1].value !== "") return;
+
+    board[cell.id-1].value = props.currentPlayer;
+
+    cell.textContent = props.currentPlayer.marker;
     
-    let nextPlayer = player === "O" ? [init.state.player2, "X"] : [init.state.player1, "O"];
+    if (winCheck.winner(props.currentPlayer)) return;
 
-    init.state.currentPlayer = nextPlayer[1]
-    init.playerElement.textContent = `${nextPlayer[0]}: ${nextPlayer[1]}`      
+    if (winCheck.tie()) return;
 
-    return;
+    gameBoard.setMark(props.currentPlayer);
+
 };
 
 const winCheck = (function() {
+    const winCondition = [
+        [1,2,3], [4,5,6], [7,8,9], [1,4,7],
+        [2,5,8], [3,6,9], [1,5,9], [3,5,7]
+    ];
+
     const winner = (player) => {
 
+        let props = gameBoard.state.properties;
+
         let win = false;
-        const playerValues = init.state.gameBoard
+        const playerValues = gameBoard.state.board
             .filter(item => item.value === player)
             .map(cell => cell.id);
 
-        for (const arr of init.winCondition) {
+        for (const arr of winCondition) {
             if (arr.every(n => playerValues.includes(n))) {
 
-                init.playerElement.textContent = 
-                    `${player === "O" 
-                        ? init.state.player1 
-                        : init.state.player2 } is the Winner!`;
+                init.playerElement.textContent = `${player.name } is the Winner!`;
                 win = true;
-                init.state.gameOver = win;
+                props.gameOver = win;
 
                 break;
             }
@@ -122,5 +136,21 @@ const winCheck = (function() {
         return win;
     }
 
-    return {winner}
+    const tie = () => {
+        let board = gameBoard.state.board
+        let props = gameBoard.state.properties;
+
+        let filledCells = board.filter(item => item.value !== "");
+
+        if (filledCells.length === 9) {
+            init.playerElement.textContent = `Game is a Tie! Click 'Restart' to play again!`;
+            props.gameOver = true;
+            
+            return true;
+        }
+
+        return false;
+    }
+
+    return {winner, tie}
 })();
